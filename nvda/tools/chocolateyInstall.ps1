ecf9e27bdc09b61bb5a7505c646fcdfac3644098
Boris Dušek <dusek@a11y.ltd.uk>
2016-04-16 17:53:31 +0200

    Do not launch NVDA after install
    Use `--install-silent` instead of `--install`. This prevents launch of
    NVDA after the install. See
    [nvda.pyw#L81](https://github.com/nvaccess/nvda/blob/1684dc53d01baa50f9c10c9a85fd2ad0cc323480/source/nvda.pyw#L81)

--- nvda/tools/chocolateyInstall.ps1
+++ nvda/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
 $packageVersion = '2016.1'
 $fileType = 'exe'
 $url = "https://www.nvaccess.org/download/nvda/releases/$packageVersion/nvda_$packageVersion.exe"
-$silentArgs = '--install'
+$silentArgs = '--install-silent'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
